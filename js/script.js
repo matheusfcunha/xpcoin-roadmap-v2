de95afafb6dbaf6b5746cc8d01337a0adc59c006
matheusfcunha <matheusfbcunha@gmail.com>
2018-04-16 16:05:41 -0300

    Fix branch issue

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 
-const   ACCORDION_BTN_TITLE = document.getElementsByClassName("accordion");
+const ACCORDION_BTN_TITLE = document.getElementsByClassName("accordion");
 const POINTER = document.getElementById("xp-pointer");
 var i;
+var j;
+
+for (j = 0; j < ACCORDION_BTN_TITLE.length; j++) {
+  ACCORDION_BTN_TITLE[j].addEventListener("mouseover", function() {
+    POINTER.style.display = "inline-block";
+    this.insertBefore(POINTER, this.firstChild);
+  })
+}
+
 for (i = 0; i < ACCORDION_BTN_TITLE.length; i++) {
-    ACCORDION_BTN_TITLE[i].addEventListener("click", function() {
+        ACCORDION_BTN_TITLE[i].addEventListener("click", function() {
+
+          this.classList.toggle("active");
+
+        for ( i = 0; i < ACCORDION_BTN_TITLE.length; i++) {
+            panel = ACCORDION_BTN_TITLE[i].nextElementSibling;
+            if (panel === this.nextElementSibling) { continue; }
+            if (panel.style.display === "block") {
+              panel.style.display = "none";
+            }
+          }
+
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
         this.classList.toggle("active");
@@ -18,5 +38,6 @@ for (i = 0; i < ACCORDION_BTN_TITLE.length; i++) {
         } else {
             panel.style.display = "block";
         }
+
     });
 }
